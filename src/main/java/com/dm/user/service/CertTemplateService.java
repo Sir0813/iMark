2a9cd57e64c8b4f81fb29cc0a5e3cccc2806dce3
cuijj <cuijj@cdisa.org>
2019-08-12 16:30:00 +0800

    模板service提交

---
+++ src/main/java/com/dm/user/service/CertTemplateService.java
@@ -0,0 +1,11 @@
+package com.dm.user.service;
+
+import com.dm.frame.jboot.msg.Result;
+import com.dm.user.entity.CertTemplate;
+
+public interface CertTemplateService {
+
+    Result list() throws Exception;
+
+    CertTemplate getByTemplateType(String type) throws Exception;
+}
---
+++ src/main/java/com/dm/user/service/impl/CertTemplateServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dm.user.service.impl;
+
+import com.dm.frame.jboot.msg.Result;
+import com.dm.frame.jboot.msg.ResultUtil;
+import com.dm.user.entity.CertTemplate;
+import com.dm.user.mapper.CertTemplateMapper;
+import com.dm.user.service.CertTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class CertTemplateServiceImpl implements CertTemplateService {
+
+    @Autowired
+    private CertTemplateMapper certTemplateMapper;
+
+    @Override
+    public Result list() throws Exception {
+        try {
+            List<CertTemplate> templateList = certTemplateMapper.list();
+            return ResultUtil.success(templateList);
+        }catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+    @Override
+    public CertTemplate getByTemplateType(String type) throws Exception {
+        try {
+            CertTemplate ct = certTemplateMapper.getByTemplateType(type);
+            return ct;
+        }catch (Exception e){
+            throw new Exception(e);
+        }
+    }
+}
